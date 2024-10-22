aad6f987a2adb644672441749acb41efc01fe3e7
Pyosch <birk-sascha@posteo.de>
2024-10-22 09:30:55 +0200

    Made code more general for different dbs

--- dashboard.py
+++ dashboard.py
@@ -23,7 +23,11 @@ import sqlite3
 import atexit
 
 # Connect to the SQL database
-conn = sqlite3.connect('data/240603_database sample.db', check_same_thread=False)
+# conn = sqlite3.connect('data/240603_database sample.db', check_same_thread=False)
+# database_table = 'merge'
+
+conn = sqlite3.connect('data/240912_inputs_online_tool.db', check_same_thread=False)
+database_table = 'inputs_online_tool'
 
 # Register a function to close the connection when the app shuts down
 def close_db():
@@ -34,7 +38,8 @@ atexit.register(close_db)
 # Fetch data from the database
 L_LIMIT = 0
 U_LIMIT = 1_000
-query = f"SELECT * FROM merge LIMIT {L_LIMIT}, {U_LIMIT}"
+query = f"SELECT * FROM {database_table} LIMIT {L_LIMIT}, {U_LIMIT}"
+# query = f"SELECT * FROM inputs_online_tool LIMIT {L_LIMIT}, {U_LIMIT}"
 df = pd.read_sql_query(query, conn)
 
 # Create the Dash app
@@ -45,11 +50,11 @@ app.layout = html.Div(
     children=[
         html.H1("VISE-D: Data Visualization Dashboard"),
         html.H3("EV Penetration:"),
-        dcc.Dropdown(options=[{'label': i, 'value': i} for i in ['30', '50']],
+        dcc.Dropdown(options=[{'label': i, 'value': i} for i in df.diffusion_evs.unique()],
                      value='30', 
                      id='ev-dropdown'),
         html.H3("Curtailment Strategy:"),
-        dcc.Dropdown(options=[{'label': i, 'value': i} for i in ['none', 'smart']],
+        dcc.Dropdown(options=[{'label': i, 'value': i} for i in df.curtailment.unique()],
                      value='none',
                      id='curtailment-dropdown'),
         dcc.Graph(id="data-graph"),
@@ -63,7 +68,7 @@ app.layout = html.Div(
      Input('curtailment-dropdown', 'value')]
 )
 def update_graph(ev_penetration, curtailment):
-    query = f"SELECT * FROM merge WHERE diffusion_evs = '{ev_penetration}' AND curtailment = '{curtailment}' LIMIT {L_LIMIT}, {U_LIMIT}"
+    query = f"SELECT * FROM {database_table} WHERE diffusion_evs = '{ev_penetration}' AND curtailment = '{curtailment}' LIMIT {L_LIMIT}, {U_LIMIT}"
     df = pd.read_sql_query(query, conn)
     fig = px.line(df, x=df.index, y='value', title=f'Data Visualization (EV Penetration: {ev_penetration}%, Curtailment: {curtailment})')
     return fig
@@ -74,7 +79,7 @@ def update_graph(ev_penetration, curtailment):
      Input('curtailment-dropdown', 'value')]
 )
 def update_violin_plot(ev_penetration, curtailment):
-    query = f"SELECT * FROM merge WHERE diffusion_evs = '{ev_penetration}' AND curtailment = '{curtailment}' LIMIT {L_LIMIT}, {U_LIMIT}"
+    query = f"SELECT * FROM {database_table} WHERE diffusion_evs = '{ev_penetration}' AND curtailment = '{curtailment}' LIMIT {L_LIMIT}, {U_LIMIT}"
     df = pd.read_sql_query(query, conn)
     fig = px.violin(df, y='value', box=True, points="all", title=f'Violin plot (EV Penetration: {ev_penetration}%, Curtailment: {curtailment})')
     return fig
