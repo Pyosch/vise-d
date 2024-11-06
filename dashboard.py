4140362f50db896e08900349f4c28f9378650d76
Pyosch <birk-sascha@posteo.de>
2024-11-06 14:05:36 +0100

    Added title and welcome text

--- dashboard.py
+++ dashboard.py
@@ -5,3 +5,7 @@ st.set_page_config(page_title='VISE-D Dashboard',
                     layout='centered',
                     # initial_sidebar_state='expanded'
                     )
+
+st.title('VISE-D Dashboard')
+
+st.write('Welcome to the VISE-D Dashboard! This is a demo of the tool developed in the VISE-D project.')
