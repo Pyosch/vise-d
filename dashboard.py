2b86fecf30c3d9563c7bfea10c0828a75cfe25bb
Pyosch <birk-sascha@posteo.de>
2024-11-06 15:06:36 +0100

    Added violin plot

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import plotly.express as px
+
 import streamlit as st
 
 st.set_page_config(page_title='VISE-D Dashboard', 
@@ -14,13 +17,13 @@ st.write('Welcome to the VISE-D Dashboard! This is a demo of the tool developed 
 with st.sidebar:
     st.title('VISE-D')
     
-    grid_type = ['Standard']
+    grid_type = ['standard']
     ev_diffusion = [30, 50, 70]
-    hp_diffusion = ['Standard']
-    pv_storage_diffusion = ['Standard']
-    curtailment = ['Keine']
-    wholesale_tariff = ['Real-time-tariff', 'Time-of-use-tariff', 'Fixed-tariff']
-    grid_usage_fees = ['Fixed', 'Variabel']
+    hp_diffusion = ['standard']
+    pv_storage_diffusion = ['standard']
+    curtailment = ['none']
+    wholesale_tariff = ['rtt', 'tou', 'fix']
+    grid_usage_fees = ['fix', 'variable']
     
     selected_grid_type = st.selectbox('Netz Typ', grid_type)
     selected_ev_diffusion = st.selectbox('EV Diffusion', ev_diffusion)
@@ -30,7 +33,43 @@ with st.sidebar:
     selected_wholesale_tariff = st.selectbox('Wholesale Tariff', wholesale_tariff)
     selected_grid_usage_fees = st.selectbox('Netznutzungsgebühren', grid_usage_fees)
     
+def update_violin_plot(ev_penetration, 
+                       curtailment,
+                       selected_grid_type, 
+                       selected_hp_diffusion, 
+                       selected_pv_storage_diffusion,
+                       selected_wholesale_tariff, 
+                       selected_grid_usage_fees):
+             
+    df = pd.read_csv('data_example.csv', index_col=0)
+    df_selected = df[(df['ev_penetration'] == ev_penetration) 
+                    & (df['curtailment'] == curtailment) 
+                    & (df['grid_type'] == selected_grid_type)
+                    & (df['hp_diffusion'] == selected_hp_diffusion)
+                    & (df['pv_storage_diffusion'] == selected_pv_storage_diffusion)
+                    & (df['wholesale_tariff'] == selected_wholesale_tariff)
+                    & (df['grid_usage_fees'] == selected_grid_usage_fees)
+                    ]
+    
+    fig = px.violin(df_selected, 
+                    y='value', 
+                    box=True, 
+                    points="all"
+                    )
+    return fig
 
+st.write('## Violin Plot')
+
+st.plotly_chart(update_violin_plot(selected_ev_diffusion, 
+                                   selected_curtailment,
+                                   selected_grid_type, 
+                                   selected_hp_diffusion, 
+                                   selected_pv_storage_diffusion,
+                                   selected_wholesale_tariff, 
+                                   selected_grid_usage_fees)
+                )
+
+# Footer with Logos
 footer_cols = st.columns(2)
 
 with footer_cols[0]:
