9f00fd77d5ac21e06c45475a54bf7c1a38085d58
Pyosch <birk-sascha@posteo.de>
2024-11-06 14:00:51 +0100

    Added page config to dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title('VISE-D Dashboard')
+st.set_page_config(page_title='VISE-D Dashboard', 
+                    page_icon=':bar_chart:',
+                    layout='centered',
+                    # initial_sidebar_state='expanded'
+                    )
